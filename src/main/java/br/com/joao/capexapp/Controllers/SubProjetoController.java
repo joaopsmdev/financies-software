f418d111ab9549750750637ac438874abd3bb47a
Joao Marinho <joaopedrosouzamarinho0@gmail.com>
2023-08-06 02:00:08 -0300

    feat business role subprojetos

--- src/main/java/br/com/joao/capexapp/Controllers/SubProjetoController.java
+++ src/main/java/br/com/joao/capexapp/Controllers/SubProjetoController.java
@@ -3,8 +3,12 @@ package br.com.joao.capexapp.Controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,13 +22,33 @@ public class SubProjetoController {
   private SubProjetoService subProjetoService;
 
   @GetMapping("/subprojeto")
-  public List<Subprojeto> get() {
-    return subProjetoService.getSubProjeto();
+  public ResponseEntity<List<Subprojeto>> get() {
+    List<Subprojeto> subprojeto = subProjetoService.getSubProjeto();
+    return ResponseEntity.status(200).body(subprojeto);
+  }
+
+  @GetMapping("/subprojeto/{id}")
+  public ResponseEntity<Subprojeto> getSubProjetobyId(@PathVariable Long id) {
+    Subprojeto subProjeto = subProjetoService.getSubProjetoById(id);
+    return ResponseEntity.status(200).body(subProjeto);
   }
 
   @PostMapping("/subprojeto")
-  public Subprojeto save(@RequestBody Subprojeto subProjeto) {
-    return subProjetoService.saveSubProjeto(subProjeto);
+  public ResponseEntity<Subprojeto> save(@RequestBody Subprojeto subProjeto) {
+    subProjetoService.saveSubProjeto(subProjeto);
+    return ResponseEntity.status(201).body(subProjeto);
+  }
+
+  @PutMapping("/subprojeto/{id}")
+  public ResponseEntity<Subprojeto> put(@PathVariable Long id, @RequestBody Subprojeto subProjeto) {
+    subProjetoService.putSubProjeto(id, subProjeto);
+    return ResponseEntity.status(201).body(subProjeto);
+  }
+
+  @DeleteMapping("/subprojeto/{id}")
+  public ResponseEntity<String> delete(@PathVariable Long id) {
+    subProjetoService.deleteSubProjeto(id);
+    return ResponseEntity.status(200).body("SubProjeto deletado com sucesso!");
   }
 
 }
--- src/main/java/br/com/joao/capexapp/Services/SubProjetoService.java
+++ src/main/java/br/com/joao/capexapp/Services/SubProjetoService.java
@@ -20,11 +20,19 @@ public class SubProjetoService {
   @Autowired
   ProjetoService projetoService;
 
+  /* GET TODOS SUBS PROJETOS */
   public List<Subprojeto> getSubProjeto() {
     List<Subprojeto> subProjeto = subProjetoRepository.findAll();
     return subProjeto;
   }
 
+  /* GET SUB PROJETO POR ID */
+  public Subprojeto getSubProjetoById(Long id) {
+    Subprojeto subProjeto = subProjetoRepository.findById(id).get();
+    return subProjeto;
+  }
+
+  /* SAVE SUB PROJETO */
   public Subprojeto saveSubProjeto(Subprojeto subProjeto) {
     if (subProjeto.getValorRealizado() == null) {
       subProjeto.setValorRealizado(0.0);
@@ -34,6 +42,33 @@ public class SubProjetoService {
     subProjeto.setValorDisponivel(subProjeto.getValorVerba() - subProjeto.getValorRealizado());
     Subprojeto subProjetos = subProjetoRepository.save(subProjeto);
     return subProjetos;
+  }
+
+  /* PUT SUB PROJETO */
+  public Subprojeto putSubProjeto(Long id, Subprojeto subProjeto) {
+    /* Pega projeto pelo id */
+    Subprojeto putSubProjeto = subProjetoRepository.findById(id).get();
+
+    /* Seta os valores do projeto */
+    putSubProjeto.setNomeSubprojeto(subProjeto.getNomeSubprojeto());
+    putSubProjeto.setValorVerba(subProjeto.getValorVerba());
+    putSubProjeto.setValorRealizado(subProjeto.getValorRealizado());
+
+    /* Verifica se o valor disponivel é maior que o valor realizado */
+    if (putSubProjeto.getValorDisponivel() != null
+        && putSubProjeto.getValorDisponivel() >= subProjeto.getValorRealizado()) {
+      putSubProjeto.setValorDisponivel(putSubProjeto.getValorVerba() - putSubProjeto.getValorRealizado());
+    } else {
+      putSubProjeto.setValorDisponivel(putSubProjeto.getValorVerba());
+    }
+    saveSubProjeto(putSubProjeto);
+    return putSubProjeto;
+  }
+
+  public String deleteSubProjeto(Long id) {
+    Subprojeto subProjeto = subProjetoRepository.findById(id).get();
+    subProjetoRepository.delete(subProjeto);
+    return "SubProjeto deletado com sucesso!";
   }
 
 }
